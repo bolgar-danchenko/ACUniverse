1b0c6979519131915b6e90798c793857c7bb3600
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-02-03 00:14:32 +0400

    Initial Commit

---
+++ ACUniverse/ACUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACUniverseApp.swift
+//  ACUniverse
+//
+//  Created by Konstantin Bolgar-Danchenko on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ACUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
